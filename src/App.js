c837c0ac33ef09f07b4b91d473c9cd0173b7f6bc
anadev <aluiza.silva22@gmail.com>
2020-11-05 17:13:08 -0500

    metatags + footer

--- src/App.js
+++ src/App.js
@@ -220,13 +220,10 @@ export default function App() {
       </aside>
       <footer>
         <p>
-          Icons made by
-          <a href="http://www.freepik.com/" title="Freepik">
-            Freepik
-          </a>
-           from 
-          <a href="https://www.flaticon.com/" title="Flaticon">
-            www.flaticon.com
+          Icons made by <a href="http://www.freepik.com/" title="Freepik">
+            Freepik 
+          </a> from <a href="https://www.flaticon.com/" title="Flaticon">
+             www.flaticon.com
           </a>
         </p>
       </footer>
